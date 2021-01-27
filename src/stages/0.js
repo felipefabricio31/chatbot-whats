ab307af122cb666b19977224ce48974cb6f09c31
Felipe Souza <f.souza@expenseon.com>
2021-01-27 08:42:06 -0300

    add projeto ao repositorio

---
+++ src/stages/0.js
@@ -0,0 +1,22 @@
+const cardapio = require("../cardapio");
+const banco = require("../banco");
+
+function execute(user, msg, contato) {
+  let menu = " CARDÁPIO \n\n";
+
+  Object.keys(cardapio.menu).forEach((value) => {
+    let element = cardapio.menu[value];
+    menu += `${value} - ${element.descricao}        R$ ${element.preco} \n`;
+  });
+
+  banco.db[user].stage = 1;
+
+  return [
+    `Olá ${contato},\n
+    Bem vindo(a) ao atendimento inteligente da Lanchonete CloudQI 😃,\n
+    para fazer o pedido basta enviar o código do produto ‼️`,
+    menu,
+  ];
+}
+
+exports.execute = execute;
