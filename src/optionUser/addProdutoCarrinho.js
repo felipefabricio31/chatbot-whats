babc69e84328d5aa7c13ccee3ad69ceea440710f
Felipe Souza <f.souza@expenseon.com>
2021-02-15 21:58:30 -0300

    Add opção para quantidade de produtos

--- src/optionUser/addProdutoCarrinho.js
+++ src/optionUser/addProdutoCarrinho.js
@@ -5,10 +5,11 @@ function execute(user, msg) {
   //Apresenta a msg de item adicionado ao carrinho
   let addItem = `*Digite a quantidade para continuar:* \n`;
   
-  //Altera o estado para o menu de opções
-  banco.db[user].stage = 5;
 
   banco.db[user].produtoEscolhido = msg;
+  
+  //Altera o estado para o menu de opções
+  banco.db[user].stage = 5;
 
   return [addItem];
 }
--- src/optionUser/addQuantidade.js
+++ src/optionUser/addQuantidade.js
@@ -4,21 +4,16 @@ const opcoesMenu = require("./opcoesMenu");
 
 function execute(user, msg) {
 
-  console.log("banco.db[user].produtoEscolhido --", banco.db[user].produtoEscolhido);
-
   let produtoEscolhido = banco.db[user].produtoEscolhido;
 
   let descricaoProduto = cardapio.menu[produtoEscolhido].descricao;
 
-  cardapio.menu[msg].quantidade = msg;
-
-  console.log("cardapio.menu[msg] --", cardapio.menu[msg]);
+  //Atribui o valor digitado ao array
+  cardapio.menu[produtoEscolhido].quantidade = msg;
 
   //Apresenta a msg de item adicionado ao carrinho
   let addItem = `*🎉 Adicionamos ${msg} - ${descricaoProduto} ao seu carrinho 🎉* \n`;
   addItem += `----------------------------------------------------- \n`;
-
-  console.log("push produto --", cardapio.menu[produtoEscolhido]);
 
   //Adiciona o item escolhido ao carrinho
   banco.db[user].itens.push(cardapio.menu[produtoEscolhido]);
